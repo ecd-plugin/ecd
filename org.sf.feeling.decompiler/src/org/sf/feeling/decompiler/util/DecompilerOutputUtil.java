57536941cf8704a1d93754a88adcd74dc2056500
nbauma109 <nbauma109@github.com>
2024-05-26 18:24:01 +0200

    DecompilerOutputUtil: use existing utility methods

--- org.sf.feeling.decompiler/src/org/sf/feeling/decompiler/util/DecompilerOutputUtil.java
+++ org.sf.feeling.decompiler/src/org/sf/feeling/decompiler/util/DecompilerOutputUtil.java
@@ -8,11 +8,7 @@
 
 package org.sf.feeling.decompiler.util;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -761,61 +757,14 @@ public class DecompilerOutputUtil {
 		}
 	}
 
-	private static String level = null;
-
 	/**
 	 * @return the maximum Java version supported by the current Eclipse JDT version
 	 */
 	public static String getMaxDecompileLevel() {
-		if (level != null) {
-			return level;
-		}
-
-		// filter oot all versions that are not a simple integers e.g. "9" "10" ...
-		Pattern p = Pattern.compile("^\\d+$");
-		List<String> allVersions = new LinkedList<>(JavaCore.getAllVersions());
-		Iterator<String> it = allVersions.iterator();
-		while (it.hasNext()) {
-			String v = it.next();
-			if (!p.matcher(v).matches()) {
-				it.remove();
-			}
-		}
-		if (allVersions.isEmpty()) {
-			level = "1.8"; //$NON-NLS-1$
-			return level;
-		}
-
-		List<Integer> allVersionsInt = new ArrayList<>();
-		for (String v : allVersions) {
-			allVersionsInt.add(Integer.parseInt(v));
-		}
-		level = Integer.toString(Collections.max(allVersionsInt));
-		return level;
+		return JavaCore.latestSupportedJavaVersion();
 	}
 
-	private static int jslLevel = -1;
-
 	public static int getMaxJSLLevel() {
-		if (jslLevel != -1) {
-			return jslLevel;
-		}
-		Pattern p = Pattern.compile("^JLS\\d+$");
-		int maxFieldValue = 8; // Java 8 is supported by all Eclipse versions ECD targets
-		for (Field f : AST.class.getFields()) {
-			if (f.getType() != int.class || !p.matcher(f.getName()).matches()) {
-				continue;
-			}
-			try {
-				int value = f.getInt(AST.class);
-				if (value > maxFieldValue) {
-					maxFieldValue = value;
-				}
-			} catch (Exception e) {
-
-			}
-		}
-		jslLevel = maxFieldValue;
-		return jslLevel;
+		return AST.getJLSLatest();
 	}
 }
